c8181ecd8dd1b70d55c959c020d7a433a821c563
dduan666 <dduan666@163.com>
2017-06-15 18:10:12 +0800

    2017-6-15 add login

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,41 +1,40 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="/css/weui.css">
     <link rel="stylesheet" href="/css/geek.css">
 </head>
 <body>
-    <div class="page">
-        @yield('content')
-    </div>
 
-    <!-- tooltips -->
-    <div class="bk_toptips"><span></span></div>
+<div class="page">
+    @yield('content')
+</div>
 
-    <div id="global_menu" onclick="onMenuClick();">
-        <div></div>
-    </div>
+<!-- tooltips -->
+<div class="bk_toptips"><span></span></div>
 
-    <!--BEGIN actionSheet-->
-    <div id="actionSheet_wrap">
-        <div class="weui_mask_transition" id="mask"></div>
-        <div class="weui_actionsheet" id="weui_actionsheet">
-            <div class="weui_actionsheet_menu">
-                <div class="weui_actionsheet_cell" onclick="onMenuItemClick(1)">用户中心</div>
-                <div class="weui_actionsheet_cell" onclick="onMenuItemClick(2)">选择套餐</div>
-                <div class="weui_actionsheet_cell" onclick="onMenuItemClick(3)">周边油站</div>
-                <div class="weui_actionsheet_cell" onclick="onMenuItemClick(4)">常见问题</div>
-            </div>
-            <div class="weui_actionsheet_action">
-                <div class="weui_actionsheet_cell" id="actionsheet_cancel">取消</div>
-            </div>
+<div id="global_menu" onclick="onMenuClick();">
+    <div></div>
+</div>
+
+<!--BEGIN actionSheet-->
+<div id="actionSheet_wrap">
+    <div class="weui_mask_transition" id="mask"></div>
+    <div class="weui_actionsheet" id="weui_actionsheet">
+        <div class="weui_actionsheet_menu">
+            <div class="weui_actionsheet_cell" onclick="onMenuItemClick(1)">用户中心</div>
+            <div class="weui_actionsheet_cell" onclick="onMenuItemClick(2)">选择套餐</div>
+            <div class="weui_actionsheet_cell" onclick="onMenuItemClick(3)">周边油站</div>
+            <div class="weui_actionsheet_cell" onclick="onMenuItemClick(4)">常见问题</div>
+        </div>
+        <div class="weui_actionsheet_action">
+            <div class="weui_actionsheet_cell" id="actionsheet_cancel">取消</div>
         </div>
     </div>
-
+</div>
 
 </body>
 <script src="/js/jquery-1.11.2.min.js"></script>
@@ -80,7 +79,6 @@
         }
     }
 </script>
-
 
 @yield('my-js')
 </html>
